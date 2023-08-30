73d9d6eb3f615e68d2d04152ab9d1717aadcefeb
Achmad Habibie F <habibiefattahillah78@gmail.com>
2023-08-30 16:30:57 +0700

    Feat(Register): Tambah register.tsx

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,16 +2,18 @@ import iconDailyHabit from "public/images/logo.png";
 import ilustration from "public/images/ilustrasi-login.png";
 import Image from "next/image";
 import Link from "next/link";
-import type { InferGetStaticPropsType, GetStaticProps, GetServerSideProps } from 'next';
+import type {
+  InferGetStaticPropsType,
+  GetStaticProps,
+  GetServerSideProps,
+} from "next";
 import { useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
 import $ from "jquery";
 import { log } from "console";
 import Loading from "@/components/loadingButton";
-import Head from 'next/head'
-
-
+import Head from "next/head";
 
 // export const getServerSideProps: GetServerSideProps = async () => {
 //   const API = process.env.API;
@@ -24,14 +26,14 @@ import Head from 'next/head'
 // };
 
 const displayNone = {
-  display: "none",  
-}
+  display: "none",
+};
 
-export default function LoginPage({API}:any) {
+export default function LoginPage({ API }: any) {
   const [isLoading, setIsLoading] = useState(false);
-  const [responseMessage, setResponseMessage] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [responseMessage, setResponseMessage] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const router = useRouter();
   // const formLogin = async (el:any) => {
   //   el.preventDefault();
@@ -43,7 +45,7 @@ export default function LoginPage({API}:any) {
   //     router.push('/dashboard');
   //   } catch (error) {
   //     const {data} = error.response;
-      
+
   //     $("#responseMessage").html(`${data.message}`);
   //     $("#responseMessage").show(300);
   //     setTimeout(() => {
@@ -62,16 +64,32 @@ export default function LoginPage({API}:any) {
       </Head>
       <div className="w-full md:w-1/2 md:px-[100px] px-10 flex justify-center items-center bg-white flex-col relative">
         <div className="flex justify-center mt-8 flex-col items-center absolute top-0">
-          <Image src="/images/logo.png" alt="Daily Healthy Habit Icon" width="80" height={0} />
-          <p className="font-semibold text-gray-500"> Daily <span className="text-ds-tosca-100">Healthy</span> Habit</p>
+          <Image
+            src="/images/logo.png"
+            alt="Daily Healthy Habit Icon"
+            width="80"
+            height={0}
+          />
+          <p className="font-semibold text-gray-500">
+            {" "}
+            Daily <span className="text-ds-tosca-100">Healthy</span> Habit
+          </p>
         </div>
-        <p className="w-full text-4xl font-bold text-ds-tosca-100 mt-24">Masuk</p>
-        <p className="w-full font-normal text-md text-gray-500">
-          Buat Perencanaan Rutinitasmu dengan Registrasi di <span className="font-semibold text-ds-tosca-100">Daily Healthy Habit</span>
+        <p className="w-full text-4xl font-bold text-ds-tosca-100 mt-24">
+          Masuk
         </p>
-        <form
-          className="my-4 w-full">
-          <div id="responseMessage" style={displayNone} className="border-red-500 text-red-500 bg-red-200 mt-2 text-center p-3 border border-green-500 bg-green-200 rounded-lg text-green-500 font-bold"></div>
+        <p className="w-full font-normal text-md text-gray-500">
+          Buat Perencanaan Rutinitasmu dengan Registrasi di{" "}
+          <span className="font-semibold text-ds-tosca-100">
+            Daily Healthy Habit
+          </span>
+        </p>
+        <form className="my-4 w-full">
+          <div
+            id="responseMessage"
+            style={displayNone}
+            className="border-red-500 text-red-500 bg-red-200 mt-2 text-center p-3 border border-green-500 bg-green-200 rounded-lg text-green-500 font-bold"
+          ></div>
           <div className="my-2 flex flex-col">
             <label htmlFor="email" className="my-1 text-ds-tosca-200 text-md">
               Email
@@ -105,16 +123,22 @@ export default function LoginPage({API}:any) {
             />
           </div>
           <div className="flex justify-end">
-              <a href="#" className="text-sm font-light text-ds-tosca-200">Lupa password?</a>
+            <a href="#" className="text-sm font-light text-ds-tosca-200">
+              Lupa password?
+            </a>
           </div>
           <div className="mt-10 flex flex-col">
-           {isLoading ? <button className="w-full text-center bg-ds-blue-100 shadow py-4 text-white font-semibold rounded-lg text-xl">
-              <Loading />
-            </button> : <button className="w-full text-center bg-ds-blue-100 shadow py-4 text-white font-semibold rounded-lg text-xl">
-              Login
-            </button>}
+            {isLoading ? (
+              <button className="w-full text-center bg-ds-blue-100 shadow py-4 text-white font-semibold rounded-lg text-xl">
+                <Loading />
+              </button>
+            ) : (
+              <button className="w-full text-center bg-ds-blue-100 shadow py-4 text-white font-semibold rounded-lg text-xl">
+                Login
+              </button>
+            )}
             <span className="text-center mt-2 font-light text-md text-gray-500">
-              Kamu belum punya akun ? yuk {" "}
+              Kamu belum punya akun ? yuk{" "}
               <Link
                 href="/register"
                 className="font-bold text-ds-blue-100 hover:text-color-cyan20"
@@ -128,24 +152,24 @@ export default function LoginPage({API}:any) {
       <div className="hidden md:block w-1/2 bg-gradient-to-tl from-ds-tosca-200 from-10% via-ds-tosca-100 via-50% to-blue-500 to-90%">
         <div className="flex h-full p-4 justify-center items-center flex-col">
           <style global jsx>{`
-              .banner {
-                position: relative;
-                animation: moveUpDown 3s linear infinite;
+            .banner {
+              position: relative;
+              animation: moveUpDown 3s linear infinite;
+            }
+            @keyframes moveUpDown {
+              0% {
+                top: 0;
               }
-              @keyframes moveUpDown {
-                0% {
-                  top: 0;
-                }
-  
-                50% {
-                  top: 30px;
-                  /* Atur ketinggian maksimal pergerakan gambar */
-                }
-  
-                100% {
-                  top: 0;
-                }
+
+              50% {
+                top: 30px;
+                /* Atur ketinggian maksimal pergerakan gambar */
               }
+
+              100% {
+                top: 0;
+              }
+            }
           `}</style>
           <Image
             src="/images/Ilustrasi-login2.png"
@@ -153,7 +177,7 @@ export default function LoginPage({API}:any) {
             width="450"
             height="450"
             className="banner"
-          />          
+          />
           <p className="font-semibold text-2xl mt-6 my-2 mt-10 text-white text-center">
             Daily <span className="text-color-cyan10">Healthy</span> Habit
           </p>
---
+++ pages/register.tsx
@@ -0,0 +1,172 @@
+import Loading from "@/components/loadingButton";
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+import React, { ChangeEvent, useState } from "react";
+
+const displayNone = {
+  display: "none",
+};
+
+export default function RegisterPage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [nama, setNama] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [password_confirmation, setPassword_confirmation] = useState("");
+
+  const InputForm = (
+    label: string,
+    type: string,
+    value: string,
+    onChange: (e: ChangeEvent<HTMLInputElement>) => void,
+    title: string,
+    placeholder: string
+  ) => {
+    return (
+      <div className="my-2 flex flex-col">
+        <label htmlFor={type} className="my-1 text-ds-tosca-200 text-md">
+          {label}
+        </label>
+        <input
+          type={type}
+          value={value}
+          onChange={onChange}
+          id="email"
+          required
+          title={title}
+          className="border-2 focus:outline-none focus:border-ds-blue-100 px-4 text-gray-500 border-gray-300 shadow p-2 py-3 rounded-lg"
+          placeholder={placeholder}
+        />
+      </div>
+    );
+  };
+
+  const formRegister = () => {
+    return (
+      <div className="h-screen w-full flex">
+        <form className="my-4 w-full">
+          {InputForm(
+            "Nama",
+            "text",
+            nama,
+            (e: ChangeEvent<HTMLInputElement>) => setNama(e.target.value),
+            "Nama harus di isi yah !",
+            "Masukan nama disini yah..."
+          )}
+          {InputForm(
+            "Email",
+            "email",
+            email,
+            (e) => setEmail(e.target.value),
+            "Email harus di isi yah !",
+            "Masukan email disini yah..."
+          )}
+          {InputForm(
+            "Password",
+            "password",
+            password,
+            (e) => setPassword(e.target.value),
+            "Password harus di isi yah !",
+            "Masukan password disini yah..."
+          )}
+          {InputForm(
+            "Konfirmasi Password",
+            "password",
+            password_confirmation,
+            (e) => setPassword_confirmation(e.target.value),
+            "Konfirmasi Password harus di isi yah !",
+            "Masukan password disini yah..."
+          )}
+          <div className="mt-10 flex flex-col">
+            {isLoading ? (
+              <button className="w-full text-center bg-ds-blue-100 shadow py-4 text-white font-semibold rounded-lg text-xl">
+                <Loading />
+              </button>
+            ) : (
+              <button className="w-full text-center bg-ds-blue-100 shadow py-4 text-white font-semibold rounded-lg text-xl">
+                Register
+              </button>
+            )}
+            <span className="text-center mt-2 font-light text-md text-gray-500">
+              Sudah punya akun ? yuk{" "}
+              <Link
+                href="/register"
+                className="font-bold text-ds-blue-100 hover:text-color-cyan20"
+              >
+                Register
+              </Link>
+            </span>
+          </div>
+        </form>
+      </div>
+    );
+  };
+
+  return (
+    <div className="min-h-screen w-full flex overflow-scroll bg-white">
+      <Head>
+        <title>Daily Habit | Register</title>
+      </Head>
+      <div className="hidden min-h-[120vh] md:block md:w-1/2 bg-gradient-to-tl from-ds-tosca-200 from-10% via-ds-tosca-100 via-50% to-blue-500 to-90%">
+        <div className="flex h-full p-4 justify-center items-center flex-col">
+          <style global jsx>{`
+            .banner {
+              position: relative;
+              animation: moveUpDown 3s linear infinite;
+            }
+            @keyframes moveUpDown {
+              0% {
+                top: 0;
+              }
+
+              50% {
+                top: 30px;
+                /* Atur ketinggian maksimal pergerakan gambar */
+              }
+
+              100% {
+                top: 0;
+              }
+            }
+          `}</style>
+          <Image
+            src="/images/ilustrasi-register.png"
+            alt="Banner Login"
+            width="450"
+            height="450"
+            className="banner"
+          />
+          <p className="font-semibold text-2xl mt-6 my-2 mt-10 text-white text-center">
+            Daily <span className="text-color-cyan10">Healthy</span> Habit
+          </p>
+          <p className="text-white font-white text-center">
+            Mulai kebiasaan baik dengan perencanaan setiap hari
+          </p>
+        </div>
+      </div>
+      <div className="w-full md:w-1/2 md:px-10 flex justify-center items-center bg-white flex-col relative">
+        <div className="flex justify-center mt-8 flex-col items-center absolute top-0">
+          <Image
+            src="/images/logo.png"
+            alt="Daily Healthy Habit Icon"
+            width="80"
+            height={0}
+          />
+          <p className="font-semibold text-gray-500">
+            {" "}
+            Daily <span className="text-ds-tosca-100">Healthy</span> Habit
+          </p>
+          <p className="w-full text-4xl font-bold text-ds-tosca-100">Daftar</p>
+          <p className="w-full font-normal text-md text-gray-500">
+            Buat Perencanaan Rutinitasmu dengan Registrasi di{" "}
+            <span className="font-semibold text-ds-tosca-100">
+              Daily Healthy Habit
+            </span>
+          </p>
+          {formRegister()}
+        </div>
+      </div>
+    </div>
+  );
+}
