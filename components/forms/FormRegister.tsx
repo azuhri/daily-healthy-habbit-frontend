8caccc87dc6d516f835af351986e271ca6c48a2e
habibiefattahillah <habibiefattahillah78@gmail.com>
2023-09-04 11:00:34 +0700

    fix(register): fix flow register langsung ke dashboard

--- components/forms/FormRegister.tsx
+++ components/forms/FormRegister.tsx
@@ -31,7 +31,7 @@ const FormRegister = () => {
         password,
         password_confirmation,
       });
-      router.push("/login");
+      router.push("/dashboard");
     } catch (error: any) {
       const { data } = error.response;
 
