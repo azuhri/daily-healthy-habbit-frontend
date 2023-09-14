b0310c51fceafd5ceb985c8bd7a642a28eb1dc31
habibiefattahillah <habibiefattahillah78@gmail.com>
2023-09-14 17:41:07 +0700

    feat(Docs): Isi Gambar & Teks

--- components/landing_page/docs/Docs.tsx
+++ components/landing_page/docs/Docs.tsx
@@ -9,9 +9,34 @@ export default function Docs() {
         <p className="text-4xl text-ds-blue-100 text-center font-bold">
           HOW TO USE APP ?
         </p>
-        <LeftAppRightText />
-        <RightAppLeftText />
-        <LeftAppRightText />
+        <LeftAppRightText
+          image="/images/fitur_home.svg"
+          imageAlt="Fitur Home"
+          title="FITUR HOME"
+          description="Pusat semua kebiasaanmu ada di sini. 
+          Lacak, kelola, dan catat setiap langkah menuju perubahan yang lebih baik. 
+          Saksikan perkembanganmu dalam riwayat kebiasaan yang informatif. 
+          Saatnya untuk mencatat, refleksi, dan mengembangkan diri!"
+        />
+        <RightAppLeftText
+          image="/images/fitur_auth.svg"
+          imageAlt="Fitur Auth"
+          title="FITUR AKUN"
+          description="Buat akun dan login untuk mulai bersama kami.
+          Dengan akun yang sama, kamu akan lebih terhubung di berbagai perangkat.
+          Lupakan kekhawatiran kehilangan data,
+          akses kebiasaanmu kapanpun dan dimanapun!"
+        />
+        <LeftAppRightText
+          image="/images/fitur_create.svg"
+          imageAlt="Fitur Create"
+          title="FITUR BUAT KEBIASAAN"
+          description="Baik belajar, olahraga, atau apapun itu,
+          daftarkan kebiasaan yang ingin anda lacak.
+          Kamu dapat atur kapan kami akan mengingatkanmu pada waktu yang tepat.
+          Jangan lupa sesuaikan warna untuk mempermudah mengenali kebiasaanmu.
+          Mulai membentuk kebiasaan yang baik, dan mulai sekarang!"
+        />
       </div>
     </>
   );
--- components/landing_page/docs/LeftAppRightText.tsx
+++ components/landing_page/docs/LeftAppRightText.tsx
@@ -1,30 +1,38 @@
 import Image from "next/image";
 
-export default function LeftAppRightText() {
+export default function LeftAppRightText({
+  image,
+  imageAlt,
+  title,
+  description,
+}: {
+  image: string;
+  imageAlt: string;
+  title: string;
+  description: string;
+}) {
   return (
     <div className="my-5">
       <div className="flex md:flex-row flex-col">
         <div className="w-full md:w-1/2 flex justify-center md:justify-start items-center relative">
-            <Image
-              data-aos="zoom-in"
-              src="/images/demo1.png"
-              alt="Daily Healthy Habit Icon"
-              width="400"
-              height={0}
-            />
+          <Image
+            data-aos="zoom-in"
+            src={image}
+            alt={imageAlt}
+            width="350"
+            height={0}
+          />
         </div>
         <div className="w-full md:w-1/2 flex justify-center flex-col">
           <div className="flex mx-8 text-justify flex-col">
-            <p className="font-semibold text-gray-500 text-3xl text-center" data-aos="zoom-in">LOREM IPSUM</p>
+            <p
+              className="font-semibold text-gray-500 text-3xl text-center"
+              data-aos="zoom-in"
+            >
+              {title}
+            </p>
             <p className="text-black text-lg my-2" data-aos="zoom-in">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Necessitatibus nihil ea adipisci architecto, velit odit modi ad
-              ducimus magnam rerum exercitationem sequi assumenda quidem
-              repudiandae eligendi libero, atque ab dolore.Lorem ipsum, dolor
-              sit amet consectetur adipisicing elit. Necessitatibus nihil ea
-              adipisci architecto, velit odit modi ad ducimus magnam rerum
-              exercitationem sequi assumenda quidem repudiandae eligendi libero,
-              atque ab dolore.
+              {description}
             </p>
           </div>
         </div>
