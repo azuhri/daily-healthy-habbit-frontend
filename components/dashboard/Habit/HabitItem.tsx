857a40854beec386ca8eb4bb8dc5f83b20db4f00
habibiefattahillah <habibiefattahillah78@gmail.com>
2023-09-22 13:53:15 +0700

    fix(habititem): add icon

--- components/dashboard/Habit/HabitItem.tsx
+++ components/dashboard/Habit/HabitItem.tsx
@@ -1,28 +1,40 @@
+import Image from "next/image";
+
 const HabitItem = () => {
   return (
-    <div className="flex rounded-full w-full bg-white h-28">
-      <div className="w-1/5 h-full bg-blue-500 rounded-l-lg"></div>
-      <div className="w-4/5 h-full bg-white rounded-r-lg text-black py-2 px-2">
+    <div className="relative flex rounded-full w-full bg-white h-28">
+      <div className="w-1/6 h-full bg-blue-500 rounded-l-lg"></div>
+      <div className="w-4/6 h-full bg-white rounded-r-lg text-black py-2 px-2">
         <h1 className="font-bold">Nama Habit</h1>
         <div className="flex space-x-2 text-xs">
           <p>• 5:00 AM</p>
           <p>• Saat ini: 8</p>
         </div>
         <p className="pt-3 text-xs line-clamp-2">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam a,
-          nemo quisquam sit similique ipsum distinctio quo, dolorem non quia
-          ducimus eum voluptate aliquam dicta mollitia totam fugit commodi
-          impedit facere consectetur modi, tempore voluptatem laudantium.
-          Consequatur consequuntur eaque laborum ad aperiam tempore,
-          necessitatibus nulla incidunt quasi sequi, quod hic aliquid voluptas,
-          eveniet odio reprehenderit. Ipsum fugit doloribus, voluptatibus dolore
-          rem est beatae sit quis iure labore earum. Dolor ad, obcaecati
-          perspiciatis, eius dolore ipsam nesciunt, nihil consequuntur
-          asperiores cum atque in debitis praesentium accusantium? Temporibus
-          quasi quidem sequi vero quo? Repellat soluta nam, cum animi assumenda
-          quod nihil illum?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex doloremque
+          ratione modi. In, ipsa ullam expedita veritatis eligendi blanditiis
+          error magnam sit? Repellendus odio ab fuga tenetur architecto soluta,
+          officiis dolor deleniti qui error aliquid possimus et eligendi porro,
+          assumenda, incidunt neque eveniet rerum consequatur hic cumque!
+          Exercitationem, quidem debitis expedita aliquam odit libero veniam
+          porro optio illo nemo. Aspernatur quae eligendi ducimus molestias quia
+          labore vel minima illum blanditiis nam molestiae dolorum corrupti
+          aperiam repellat ab neque ex aut, atque voluptatum nostrum
+          necessitatibus minus iusto dicta sit. Sequi nemo blanditiis, qui
+          laudantium deserunt dignissimos corrupti illum enim voluptatum cumque.
         </p>
       </div>
+      <div className="w-1/6 bg-white flex justify-center items-center text-black rounded-r-lg">
+        <Image
+          src="/icons/status-sukses.svg"
+          alt="icon sukses"
+          width={40}
+          height={40}
+        />
+      </div>
+      <button className="absolute top-2 right-2 bg-white">
+        <Image src="/icons/edit.svg" alt="your icon" width={25} height={25} />
+      </button>
     </div>
   );
 };
