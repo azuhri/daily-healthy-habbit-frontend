d3d9eed1f7f9e3420eabd246e4e04006cc68d3c3
habibiefattahillah <habibiefattahillah78@gmail.com>
2023-10-05 02:25:54 +0700

    fix(login/reg): handle error + tombol showpassword

--- pages/api/login.tsx
+++ pages/api/login.tsx
@@ -7,30 +7,45 @@ export default withSessionRoute(async function handler(req, res) {
       const { email, password } = req.body;
       const API = process.env.API;
       try {
-          const response = await axios.post(`${API}/api/v1/login`, {
-            email, password
-          });
-          // console.log(response);
-          
-          const dataUser = {
-            id: response.data.data.id,
-            name: response.data.data.name,
-            email: response.data.data.email,
-            phonenumber: response.data.data.phonenumber,
-            token: response.data.data.access_token,
-          }
-          // console.log(dataUser);
-          
-          req.session.user = dataUser;
-          await req.session.save();
-          res.status(200).json({
-            "message": response.data.message,
-          })
-      } catch (error:any) {
-        const errorResponse = error.response.data;
-        res.status(400).json({
-            "message": errorResponse.message,
-            "status": false
+        const response = await axios.post(`${API}/api/v1/login`, {
+          email,
+          password,
+        });
+        // console.log(response);
+
+        const dataUser = {
+          id: response.data.data.id,
+          name: response.data.data.name,
+          email: response.data.data.email,
+          phonenumber: response.data.data.phonenumber,
+          token: response.data.data.access_token,
+        };
+        // console.log(dataUser);
+
+        req.session.user = dataUser;
+        await req.session.save();
+        res.status(200).json({
+          message: response.data.message,
+        });
+      } catch (error: any) {
+        const { data } = error.response;
+        console.log(data);
+
+        let customMessage = data.message;
+        let statusCode = 400;
+        if (data.errors.email) {
+          customMessage = data.errors.email[0];
+        }
+
+        if (data.errors.password) {
+          customMessage = data.errors.password[0];
+        }
+
+        if (customMessage === "These credentials do not match our records.")
+          customMessage = "Email atau Password salah";
+
+        res.status(statusCode).json({
+          message: customMessage,
         });
       }
       break;
